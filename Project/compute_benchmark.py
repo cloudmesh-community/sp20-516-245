7486a02ea5134b84940351ca79501abef537c009
baker203 <baker203@iu.edu>
2020-04-26 19:48:44 -0400

    Use classes to add robustness

--- Project/compute_benchmark.py
+++ Project/compute_benchmark.py
@@ -15,11 +15,62 @@ from cloudmesh.configuration.Config import Config
 from cloudmesh.management.configuration.name import Name
 from cloudmesh.mongo.CmDatabase import CmDatabase
 
-tmp = [
+class ComputeBenchmark(object):
+
+    def __init__(self, provider_name, function_name, setup_function=None, validation=None):
+        try:
+            self.provider_name = provider_name
+            self.function_name = function_name
+
+            self.test_name = f"test_{provider_name}_{function_name}"
+
+            self.provider = Provider(name=provider_name)
+            #Gets the implementation tied to a specific provider as a wrapped function, so it can be invoked directly, without needing to pass a self object\
+            self.function = getattr(self.provider, function_name) 
+
+            self._sf = setup_function if setup_function else self.no_setup
+            self._vf = validation if validation else self.validate_result_exists
+
+            self.result = ''
+            self.status = False
+
+        except Exception as e:
+            print(e)
+            self.result = f"{provider_name} {function_name} {False}"
+
+    def no_setup(self):
+        return
+
+    def validate_result_exists(self, result):
+        return result != None
+
+    def setup_function(self):
+        return self._sf()
+
+    def validation_function(self, data):
+        return self._vf(data)
+
+# "{provider} {label} {start} {end} {elapsed} {status} {newline}"
+class ComputeBenchmarkOutput(object):
+    def __init__(self, provider, function, start, end, status, excpection=None):
+        self.provider = provider
+        self.function = function
+        self.start = start
+        self.end = end
+        self.status = status
+        self.exception = excpection
+        self.duration = int(float(end) - float(start))
+
+    def __str__(self):
+        return f"{self.provider} {self.function} {self.start} {self.end} {self.duration} {self.status} {self.exception}"
+
+    def __repr__(self):
+        return f"{self.provider} {self.function} {self.start} {self.end} {self.duration} {self.status} {self.exception}"
+
+functionality_benchmark = [
     "list", 
     "images", 
     "flavors", 
-
     "start", 
     "ping", 
     "terminate", 
@@ -30,108 +81,76 @@ tmp = [
     "ssh",
     "keys",
     "stop", 
+] 
 
-]
+providers = Provider.get_kind() 
 
-functionality_benchmark = ['list', 'images', 'flavors', 'create', 'stop']
-#'create' needs cms set cloud={} 
+def test_function(benchmark_obj):
 
-def get_providers():
-    clouds = ["google", "azure"] # Provider.get_kind()
-    providers = []
-    for cloud in clouds:
-        try:
-            p = Provider(name=cloud)
-            providers.append(p) 
-        except Exception as e:
-            #print(e)
-            #print("error getting cloud provider for : {}".format(cloud))
-            continue
-    return providers
+    os.system(f"cms set cloud={benchmark_obj.provider_name}")
+    thrown = None
 
-#TODO take in function for ensuring command has proper result
-def test_function(provider, function, function_name, setup_function, validation_function):
+    benchmark_obj.setup_function()
 
-    test_name = f"test_{function_name}"
-
-    variables = Variables()
-    VERBOSE(variables.dict())
-
-    key = variables['key']
-
-
-
-    setup_function()
-
-    StopWatch.start(test_name) 
+    StopWatch.start(benchmark_obj.test_name) 
     try:
-        data = function() #TODO allow inputs from kwargs too #stop, start need image name
-        StopWatch.stop(test_name)
+        data = benchmark_obj.function() #TODO allow inputs from args/kwargs too #e.g. stop, start need image name
+        StopWatch.stop(benchmark_obj.test_name)
         
-        valid = validation_function(data)
+        valid = benchmark_obj.validation_function(data)
         results = StopWatch.__str__()
 
-    except IndentationError as e:
+    except Exception as e:
         print(e)
-        StopWatch.stop(test_name)
+        StopWatch.stop(benchmark_obj.test_name)
         valid = False
-        results = e
+        thrown = e
+
+    sw = StopWatch.__str__().split(" ")
+
+    result = ComputeBenchmarkOutput(
+        benchmark_obj.provider_name, 
+        benchmark_obj.function_name,
+        sw[1],
+        sw[2],
+        valid,
+        thrown if thrown else None
+    )
     
-    results = f"{provider.name} {valid} {results}"
-    return results
-
-def no_setup():
-    return
-
-def validate_result_exists(result):
-    return result != None
+    return result
 
 def main():
-    #validators = {"list": validate_list}
-
+    Benchmark.debug()
     user = Config()["cloudmesh.profile.user"]
 
     name_generator = Name()
     name_generator.set(f"benchmark-{user}-vm-" + "{counter}")
-    #name_generator.incr()
+    name_generator.incr()
 
-
-    providers = get_providers()
     results = {}
 
     for provider in providers:
-        provider_name = provider.cloudname()
-
-        print(provider_name)
-
-        os.system(f"cms set cloud={provider_name}")
-
-        results[provider_name] = {}
+        results[provider] = {}
 
         for f_name in functionality_benchmark:
+            results[provider][f_name] = []
+
+            x = ComputeBenchmark(provider, f_name)
+
             try:
+                results[provider][f_name].append(test_function(x))
 
-                #validation = validators[f_name]
-                validation = validate_result_exists
-                
-                #Gets the implementation tied to a specific provider as a wrapped function, so it can be invoked directly, without needing to pass a self object
-                func = getattr(provider, f_name) 
-                result = test_function(provider, func, f_name, no_setup, validation)
-
-                results[provider.name][f_name] = result
-            except IndentationError as e:
+            except Exception as e:
                 print(e)
 
+    #TODO use Tabulate writer to print
     print(results)
         
-    with open('benchmark.txt', 'w') as f:
-        print(results, file=f)
-
-    #TODO process data and write to file
-    # "{provider} {label} {start} {end} {elapsed} {status} {newline}"
+    with open('benchmark.txt', 'w'  ) as f:
+        for provider, functions in results.items():
+            for function in functions.keys():
+                for result in functions[function]:
+                    f.write(f"{result.__str__()}\n")
 
 if __name__ == '__main__':
-    Benchmark.debug()
-    #user = Config()["cloudmesh.profile.user"]
-
     main()
